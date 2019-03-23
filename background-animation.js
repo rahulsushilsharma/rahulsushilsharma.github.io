19dc7c2e4881d413b0d30eea95d3737bbad5b241
Rahul Sharma <rahu8299@gmail.com>
2019-03-23 15:31:40 +0530

    reparing the background annimation error

--- background-animation.js
+++ background-animation.js
@@ -1,11 +1,11 @@
-//var canvas;
+var canvas;
 
 function setup() {
 	// body...
-	// canvas = createCanvas(windowWidth,windowHeight);
-	// canvas.position(0,0);
-	// canvas.style('z-index','-1');
-	createCanvas(200,200);
+	canvas = createCanvas(windowWidth,windowHeight);
+	canvas.position(0,0);
+	canvas.style('z-index','-1');
+
 }
 
 function draw(){
@@ -13,6 +13,6 @@ function draw(){
 
 }
 
-// function windowResized(){
-// 	resizeCanvas();
-// }
+function windowResized(){
+	resizeCanvas();
+}
